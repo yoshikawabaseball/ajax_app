71ff540bf04aec0133d4a4b0330d9602ce049625
yoshikawabaseball <zhixingt9@gmail.com>
2020-08-15 22:19:45 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
 
   def create
     Post.create(content: params[:content])
+    redirct_to acton: :index
   end
 end
 
